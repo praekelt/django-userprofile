041100c91040a89a9d55193c65aa56b5a91d5e24
Shaun Sephton <shaun@28lines.com>
2012-05-13 11:44:21 +0200

    much better get_profile method

--- userprofile/utils.py
+++ userprofile/utils.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.db.models.loading import get_model
 
 
 def get_profile_model():
@@ -6,11 +7,8 @@ def get_profile_model():
     Returns configured user profile model or None if not found
     """
     user_profile_module = getattr(settings, 'USER_PROFILE_MODULE', None)
-    profile_model = None
     if user_profile_module:
-        # get the profile model. TODO: super flacky, refactor
-        app_label, model = user_profile_module.split('.')
-        profile_model = getattr(__import__("%s.models" % app_label, \
-                globals(), locals(), [model, ], -1), model, None)
-
-    return profile_model
+        app_label, model_name = user_profile_module.split('.')
+        return get_model(app_label, model_name)
+    else:
+        return None
