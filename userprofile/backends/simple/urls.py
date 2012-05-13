cf48db41a962f6c72eddf0aa9ab69d16526a4034
Shaun Sephton <shaun@28lines.com>
2012-05-13 11:44:41 +0200

    userprofile rename

--- userprofile/backends/simple/urls.py
+++ userprofile/backends/simple/urls.py
@@ -7,6 +7,6 @@ from registration.views import register
 urlpatterns = patterns('',
     url(r'^register/$',
     register,
-    {'backend': 'profile.backends.simple.SimpleBackend'},
+    {'backend': 'userprofile.backends.simple.SimpleBackend'},
     name='registration_register'),
 )
