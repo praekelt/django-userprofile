3657425a5e4eca843130e8016e017dfa4af84cb8
Shaun Sephton <shaun@28lines.com>
2012-05-29 11:29:47 +0200

    0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-userprofile',
-    version='0.0.5',
+    version='0.0.6',
     description='Django user profile app.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
